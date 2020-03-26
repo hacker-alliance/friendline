8f9999934d2289bc96173f33c5f400ec265553cc
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-03-25 22:39:22 -0400

    Apply ESLint for Code Formatting

--- Backend/incoming/app.js
+++ Backend/incoming/app.js
@@ -6,65 +6,68 @@ AWS.config.update({ region: 'us-east-1' });
 
 const DynamoDB = new AWS.DynamoDB();
 
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const { VoiceResponse } = require('twilio').twiml;
 
 let response;
-
-const updateParams = {
-    Key: {
-        QUEUE_ID: { N: '001' },
-    },
-    ExpressionAttributeNames: {
-        "#C": "Caller_ID"
-    },
-    ExpressionAttributeValues: {
-        ":incr": { "N": "1" }
-    },
-    ReturnValues: "ALL_NEW",
-    TableName: "QueueTable",
-    UpdateExpression: "SET #C = #C + :incr"
-};
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    const twiml = new VoiceResponse();
-    twiml.say({ voice: 'alice' }, 'Hello, Welcome to neighbor line!');
+  console.log(event);
+  console.log(context);
 
-    twiml.say({ voice: 'alice' }, 'We\'ll be pairing you with a neighbor shortly, here\'s some music while you wait');
+  const updateParams = {
+    Key: {
+      QUEUE_ID: { N: '001' },
+    },
+    ExpressionAttributeNames: {
+      '#C': 'Caller_ID',
+    },
+    ExpressionAttributeValues: {
+      ':incr': { N: '1' },
+    },
+    ReturnValues: 'ALL_NEW',
+    TableName: 'QueueTable',
+    UpdateExpression: 'SET #C = #C + :incr',
+  };
 
-    let item = await DynamoDB.updateItem(updateParams).promise();
-    console.log(item);
-    let caller_ID = item.Attributes.Caller_ID.N;
+  const twiml = new VoiceResponse();
+  twiml.say({ voice: 'alice' }, 'Hello, Welcome to neighbor line!');
 
-    const dial = twiml.dial();
+  twiml.say({ voice: 'alice' }, 'We\'ll be pairing you with a neighbor shortly, here\'s some music while you wait');
 
-    let room_ID = (caller_ID % 2 == 0) ? caller_ID - 1 : caller_ID;
+  const item = await DynamoDB.updateItem(updateParams).promise();
+  console.log(item);
+  const callerID = item.Attributes.Caller_ID.N;
 
-    dial.conference({
-        waitUrl: 'https://api.neighborline.hackeralliance.org/wait/music',
-        beep: true
-    }, 'Pair_Room_' + room_ID);
+  const dial = twiml.dial();
 
-    try {
-        response = {
-            'statusCode': 200,
-            'headers': { "content-type": "text/xml" },
-            'body': twiml.toString()
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-    return response
+  const roomID = (callerID % 2 === 0) ? callerID - 1 : callerID;
+
+  dial.conference({
+    waitUrl: 'https://api.neighborline.hackeralliance.org/wait/music',
+    beep: true,
+  }, `Pair_Room_${roomID}`);
+
+  try {
+    response = {
+      statusCode: 200,
+      headers: { 'content-type': 'text/xml' },
+      body: twiml.toString(),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+  return response;
 };
--- Backend/wait/app.js
+++ Backend/wait/app.js
@@ -1,4 +1,4 @@
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const { VoiceResponse } = require('twilio').twiml;
 
 let response;
 
@@ -7,29 +7,32 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    const twiml = new VoiceResponse();
+  console.log(event);
+  console.log(context);
 
-    twiml.play('http://demo.twilio.com/docs/classic.mp3');
-    twiml.say({ voice: 'alice' }, 'Thank you for waiting. We\'ll be pairing you with a neighbor shortly.');
-    twiml.redirect();
+  const twiml = new VoiceResponse();
 
-    try {
-        response = {
-            'statusCode': 200,
-            'headers': { "content-type": "text/xml" },
-            'body': twiml.toString()
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-    return response
+  twiml.play('http://demo.twilio.com/docs/classic.mp3');
+  twiml.say({ voice: 'alice' }, 'Thank you for waiting. We\'ll be pairing you with a neighbor shortly.');
+  twiml.redirect();
+
+  try {
+    response = {
+      statusCode: 200,
+      headers: { 'content-type': 'text/xml' },
+      body: twiml.toString(),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+  return response;
 };
